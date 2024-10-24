05f1f256d399e96509e533b2c46f013476143bbb
Mr0riginal <89471673+Mr0riginal@users.noreply.github.com>
2024-10-24 15:18:17 +0530

    initial commit

---
+++ object.js
@@ -0,0 +1,16 @@
+const person=[
+    {
+        name:"Amit",
+        roll:22
+    },
+
+    {
+        name:"Rohit",
+        roll:23
+    },
+
+    {
+        name:"Rahul",
+        roll:24
+    }
+]
